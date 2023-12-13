0186c727be049fcec8de409de0bd78ac937d52c0
HorstDev <gorstsergei@mail.ru>
2023-12-13 18:04:43 +0300

    added tokens and hashing

---
+++ AcademicProgressTracker.Application/Auth/RefreshToken.cs
@@ -0,0 +1,24 @@
+﻿using AcademicProgressTracker.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicProgressTracker.Application.Auth
+{
+    public class RefreshToken : IToken
+    {
+        public string Token { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Expires { get; set; }
+
+        public RefreshToken(int lifeTimeInDays)
+        {
+            Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64));
+            Expires = DateTime.Now.AddDays(lifeTimeInDays);
+            Created = DateTime.Now;
+        }
+    }
+}
