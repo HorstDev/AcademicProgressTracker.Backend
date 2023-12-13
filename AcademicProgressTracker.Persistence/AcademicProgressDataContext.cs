9f5c42c456847bcaa4eb1283548511cba7953ae4
HorstDev <gorstsergei@mail.ru>
2023-12-13 16:46:28 +0300

    added connection to the postgresql database

---
+++ AcademicProgressTracker.Persistence/AcademicProgressDataContext.cs
@@ -0,0 +1,42 @@
+﻿using AcademicProgressTracker.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicProgressTracker.Persistence
+{
+    public class AcademicProgressDataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        public AcademicProgressDataContext(DbContextOptions<AcademicProgressDataContext> options) 
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Role>().HasData(AllRoles());
+
+            modelBuilder.UseSerialColumns();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private Role[] AllRoles()
+        {
+            const string studentRoleName = "Student";
+            const string teacherRoleName = "Teacher";
+            const string adminRoleName = "Admin";
+
+            var studentRole = new Role { Id = Guid.NewGuid(), Name = studentRoleName };
+            var teacherRole = new Role { Id = Guid.NewGuid(), Name = teacherRoleName };
+            var adminRole = new Role { Id = Guid.NewGuid(), Name = adminRoleName };
+
+            return new Role[] { studentRole, teacherRole, adminRole };
+        }
+    }
+}
--- AcademicProgressTracker.WebApi/Program.cs
+++ AcademicProgressTracker.WebApi/Program.cs
@@ -1,6 +1,14 @@
+using AcademicProgressTracker.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AcademicProgressDataContext>(
+    o => o.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+    );
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
