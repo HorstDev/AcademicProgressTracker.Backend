bf62970267bf133cf3fcc0366f9abfce20b106ba
Sergey <gorstsergei@mail.ru>
2024-02-06 17:01:26 +0300

    added group and subject view models

---
+++ AcademicProgressTracker.Application/Common/ViewModels/Subject/SubjectViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AcademicProgressTracker.Application.Common.ViewModels.Subject
+{
+    public class SubjectViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
